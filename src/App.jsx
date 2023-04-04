aa1721406b8e35cd0361ad230c8a06151266fdb2
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-04-04 23:47:43 +0530

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import React from 'react'
+import Namerize from './pages/namerize'
 
 function App() {
-
-
   return (
-    <div className="App">
-
-
-    </div>
+    <div className="w-full sm:px-8 bg-gradient-to-br from-[#EF3B36] via-[#ffdde1ef]">
+      <header className="flex items-center px-6 py-6">
+        <div>
+          <h1 className="text-2xl font-bold"
+          >namerize</h1>
+        </div>
+      </header>
+      <main className="px-6 py-6">
+        <Namerize />
+      </main>
+      <footer>
+        <div>
+          <p className="text-center text-gray-500 text-xs">
+            2023 namerize. All rights reserved.
+          </p>
+        </div>
+      </footer>
+      </div>
   )
 }
 
---
+++ src/pages/namerize.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function namerize() {
+  return (
+    <div>
+      <h1>
+        namerize
+      </h1>
+    </div>
+  )
+}
+
+export default namerize
