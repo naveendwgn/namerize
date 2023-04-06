511c6ec15f995726b8558c203f07eec1c633a7d4
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-04-06 23:53:04 +0530

    halfway

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import Namerize from './pages/namerize'
+import { FaGithub } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="w-full sm:px-8 bg-gradient-to-br from-[#EF3B36] via-[#ffdde1ef]">
-      <header className="flex items-center px-6 py-6">
-        <div>
+    <div className="w-full sm:px-8 bg-[#000]">
+      <header className="flex flex-wrap justify-between items-center px-6 py-6">
           <h1 className="text-2xl font-bold"
           >namerize</h1>
-        </div>
+
+          <a href="" className="text-3xl ml-4">
+            <FaGithub />
+          </a>
+
       </header>
-      <main className="px-6 py-6">
+      <main className="w-full h-screen">
         <Namerize />
       </main>
-      <footer>
+      <footer className='flex items-center justify-center w-full h-24'>
         <div>
           <p className="text-center text-gray-500 text-xs">
             2023 namerize. All rights reserved.
--- src/pages/namerize.jsx
+++ src/pages/namerize.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 function namerize() {
   return (
-    <div>
-      <h1>
-        namerize
+    <div className="px-6 py-12">
+      <h1 className='text-5xl font-bold text-center mb-8'>
+      Generate Perfect Product Names with Namerize
       </h1>
+      <p className='text-lg text-gray-400 text-center mt-4'>
+      Struggling to find the perfect name for your next project or business?
+      </p>
+      <p className='text-md text-gray-400 text-center mt-4'>
+      Namerize has got you covered.
+      Namerize is a free tool that helps you generate perfect product names. It is a simple and easy to use tool that helps you generate product names for your business.
+      </p>
     </div>
   )
 }
